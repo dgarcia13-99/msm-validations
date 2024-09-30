7393bec64805468ee2ccee219c67f02d07a46dcc
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-30 21:23:19 +0000

    validation tests inside movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,6 +13,9 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  validates(:director_id, presence: true)
+  validates(:title, uniqueness:true)
+  
   def director
     my_director_id = self.director_id
 
@@ -22,4 +25,5 @@ class Movie < ApplicationRecord
 
     return the_director
   end
+
 end
